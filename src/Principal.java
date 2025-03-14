6020ffedd93f74ceeb7ac6d8fc982f08168bb637
Alumno UEM <alumno@N205-03.alumnos.uem.es>
2025-03-14 13:05:51 +0100

    M3

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 
 		System.out.println("Hola a tod@s");
+		System.out.println("Examen de Entornos de Desarrollo");
+		System.out.println("Examen de ED Trimestre 2");
 		
 	}
 	
